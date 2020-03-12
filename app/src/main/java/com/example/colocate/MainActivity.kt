6cae276543792537fda2f24680ce8f4f68a2ca44
Anon 10 <anon10@example.com>
2020-03-12 13:58:23 +0000

    Fix link color and target.
    Signed-off-by: M S <mshamsi@pivotal.io>

--- app/src/main/java/com/example/colocate/MainActivity.kt
+++ app/src/main/java/com/example/colocate/MainActivity.kt
@@ -1,10 +1,6 @@
 package com.example.colocate
 
 import android.os.Bundle
-import android.text.Html
-import android.text.Html.FROM_HTML_MODE_COMPACT
-import android.text.method.LinkMovementMethod
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -12,8 +8,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val link = this.findViewById<TextView>(R.id.explanation_link)
-        link.text = Html.fromHtml(getString(R.string.explanation_link), FROM_HTML_MODE_COMPACT)
-        link.movementMethod = LinkMovementMethod.getInstance()
     }
 }
