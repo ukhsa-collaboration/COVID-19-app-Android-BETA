8d77b79fcc8b091436eb8e024caa92784b9046aa
Anon 10 <anon10@example.com>
2020-03-19 17:38:43 +0000

    Allow user to re-diagnose.

--- app/src/main/java/com/example/colocate/OkActivity.kt
+++ app/src/main/java/com/example/colocate/OkActivity.kt
@@ -1,11 +1,17 @@
 package com.example.colocate
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
 
 class OkActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ok)
+
+        findViewById<AppCompatButton>(R.id.re_diagnose_button).setOnClickListener {
+            startActivity(Intent(this, DiagnoseActivity::class.java))
+        }
     }
 }
