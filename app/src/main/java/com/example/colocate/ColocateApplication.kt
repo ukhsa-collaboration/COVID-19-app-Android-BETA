66e2a31bf766325bf0d68ddae2f66daa9a82e793
Damien Le Berrigaud <git@itsdamo.com>
2020-03-31 14:20:02 -0600

    Remove unused code.

--- app/src/main/java/com/example/colocate/ColocateApplication.kt
+++ app/src/main/java/com/example/colocate/ColocateApplication.kt
@@ -5,11 +5,6 @@
 package com.example.colocate
 
 import android.app.Application
-import androidx.work.BackoffPolicy
-import androidx.work.Constraints
-import androidx.work.NetworkType
-import androidx.work.OneTimeWorkRequestBuilder
-import androidx.work.WorkManager
 import com.example.colocate.di.ApplicationComponent
 import com.example.colocate.di.DaggerApplicationComponent
 import com.example.colocate.di.module.AppModule
@@ -18,10 +13,8 @@ import com.example.colocate.di.module.NetworkModule
 import com.example.colocate.di.module.PersistenceModule
 import com.example.colocate.di.module.RegistrationModule
 import com.example.colocate.di.module.StatusModule
-import com.example.colocate.registration.RegistrationWorker
 import timber.log.Timber
 import uk.nhs.nhsx.sonar.android.client.di.EncryptionKeyStorageModule
-import java.util.concurrent.TimeUnit
 
 const val BASE_URL = "https://sonar-colocate-services.apps.cp.data.england.nhs.uk"
 
@@ -45,17 +38,5 @@ class ColocateApplication : Application() {
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
         }
-    }
-
-    private fun enqueueRegistration() {
-        val constraints = Constraints.Builder()
-            .setRequiredNetworkType(NetworkType.CONNECTED)
-            .build()
-
-        val registerRequest = OneTimeWorkRequestBuilder<RegistrationWorker>()
-            .setConstraints(constraints)
-            .setBackoffCriteria(BackoffPolicy.LINEAR, 1, TimeUnit.MINUTES)
-            .build()
-        WorkManager.getInstance(this@ColocateApplication).enqueue(registerRequest)
     }
 }
