d3e71547899d08fb0f81999a4c89e038e2e75eee
Damien Le Berrigaud <git@itsdamo.com>
2020-04-01 11:07:31 -0600

    Remove unused code.

--- app/src/main/java/com/example/colocate/di/module/BluetoothModule.kt
+++ app/src/main/java/com/example/colocate/di/module/BluetoothModule.kt
@@ -6,7 +6,6 @@ package com.example.colocate.di.module
 
 import android.bluetooth.BluetoothManager
 import android.bluetooth.le.BluetoothLeAdvertiser
-import android.bluetooth.le.BluetoothLeScanner
 import android.content.Context
 import androidx.core.content.ContextCompat.getSystemService
 import com.polidea.rxandroidble2.RxBleClient
@@ -18,10 +17,6 @@ class BluetoothModule(private val applicationContext: Context) {
     @Provides
     fun provideBluetoothManager(): BluetoothManager =
         getSystemService(applicationContext, BluetoothManager::class.java)!!
-
-    @Provides
-    fun provideBluetoothScanner(bluetoothManager: BluetoothManager): BluetoothLeScanner =
-        bluetoothManager.adapter.bluetoothLeScanner
 
     @Provides
     fun provideBluetoothAdvertiser(bluetoothManager: BluetoothManager): BluetoothLeAdvertiser =
