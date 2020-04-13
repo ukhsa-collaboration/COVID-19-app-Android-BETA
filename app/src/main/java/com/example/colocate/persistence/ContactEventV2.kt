2ad3d2863c3ba612adbab2b1e2ca67d2074f2983
Anon 10 <anon10@example.com>
2020-04-13 18:10:01 +0100

    Ensure ContactEventV2 is still a data class.

--- app/src/main/java/com/example/colocate/persistence/ContactEventV2.kt
+++ app/src/main/java/com/example/colocate/persistence/ContactEventV2.kt
@@ -7,7 +7,7 @@ import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 @Entity(tableName = ContactEventV2.TABLE_NAME)
-class ContactEventV2(
+data class ContactEventV2(
     @PrimaryKey(autoGenerate = true)
     val id: Long = 0,
     @ColumnInfo(typeAffinity = ColumnInfo.BLOB)
@@ -18,5 +18,29 @@ class ContactEventV2(
 ) {
     companion object {
         const val TABLE_NAME = "contactEventsV2"
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as ContactEventV2
+
+        if (id != other.id) return false
+        if (!sonarId.contentEquals(other.sonarId)) return false
+        if (rssiValues != other.rssiValues) return false
+        if (timestamp != other.timestamp) return false
+        if (duration != other.duration) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id.hashCode()
+        result = 31 * result + sonarId.contentHashCode()
+        result = 31 * result + rssiValues.hashCode()
+        result = 31 * result + timestamp.hashCode()
+        result = 31 * result + duration.hashCode()
+        return result
     }
 }
