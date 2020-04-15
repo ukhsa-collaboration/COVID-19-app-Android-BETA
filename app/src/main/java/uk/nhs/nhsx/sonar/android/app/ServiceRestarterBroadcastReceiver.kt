731c9a4edea4e51553b064ca846af62066de24cb
Damien Le Berrigaud <git@itsdamo.com>
2020-04-15 16:01:00 -0600

    Use BluetoothService.start

--- app/src/main/java/uk/nhs/nhsx/sonar/android/app/ServiceRestarterBroadcastReceiver.kt
+++ app/src/main/java/uk/nhs/nhsx/sonar/android/app/ServiceRestarterBroadcastReceiver.kt
@@ -3,7 +3,6 @@ package uk.nhs.nhsx.sonar.android.app
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import androidx.core.content.ContextCompat
 import timber.log.Timber
 import uk.nhs.nhsx.sonar.android.app.ble.BluetoothService
 
@@ -11,10 +10,7 @@ class ServiceRestarterBroadcastReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         Timber.d("onReceive $intent")
         if (intent.action == ACTION_RESTART_BLUETOOTH_SERVICE) {
-            ContextCompat.startForegroundService(
-                context,
-                Intent(context, BluetoothService::class.java)
-            )
+            BluetoothService.start(context)
         }
     }
 
