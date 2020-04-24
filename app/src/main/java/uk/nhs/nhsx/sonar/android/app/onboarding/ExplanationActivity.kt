0b40d529099b3d5e449301b8c73caee60b016fe4
Anon 15 <anon15@example.com>
2020-04-24 12:55:18 -0500

    Enable Landscape mode in a few screens missed earlier. Add hyperlinks and update text in explanation screen.

--- app/src/main/java/uk/nhs/nhsx/sonar/android/app/onboarding/ExplanationActivity.kt
+++ app/src/main/java/uk/nhs/nhsx/sonar/android/app/onboarding/ExplanationActivity.kt
@@ -7,13 +7,22 @@ package uk.nhs.nhsx.sonar.android.app.onboarding
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.text.HtmlCompat
+import kotlinx.android.synthetic.main.activity_explanation.explanationHow
 import kotlinx.android.synthetic.main.activity_explanation.explanation_back
 import uk.nhs.nhsx.sonar.android.app.R
 
 class ExplanationActivity : AppCompatActivity(R.layout.activity_explanation) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        explanationHow.text = HtmlCompat.fromHtml(
+            getString(R.string.explanation_how),
+            HtmlCompat.FROM_HTML_MODE_LEGACY
+        )
+        explanationHow.movementMethod = LinkMovementMethod.getInstance()
+
         explanation_back.setOnClickListener {
             onBackPressed()
         }
