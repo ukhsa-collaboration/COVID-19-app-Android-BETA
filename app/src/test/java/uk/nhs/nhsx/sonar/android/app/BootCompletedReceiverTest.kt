185862ba2deb0adab30e3cde756a5e2550e999a7
Anon 19 <anon19@example.com>
2020-04-28 15:12:06 +0100

    Add tests for BootCompletedReceiver

--- app/src/test/java/uk/nhs/nhsx/sonar/android/app/BootCompletedReceiverTest.kt
+++ app/src/test/java/uk/nhs/nhsx/sonar/android/app/BootCompletedReceiverTest.kt
@@ -6,19 +6,25 @@ package uk.nhs.nhsx.sonar.android.app
 
 import android.content.Context
 import android.content.Intent
+import io.mockk.Called
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.mockkObject
 import io.mockk.verify
+import io.mockk.verifyAll
 import org.junit.Before
 import org.junit.Test
+import uk.nhs.nhsx.sonar.android.app.ble.BluetoothService
 import uk.nhs.nhsx.sonar.android.app.di.ApplicationComponent
 import uk.nhs.nhsx.sonar.android.app.notifications.ReminderManager
+import uk.nhs.nhsx.sonar.android.app.registration.SonarIdProvider
 
 class BootCompletedReceiverTest {
 
     private val reminderManager = mockk<ReminderManager>(relaxed = true)
     private val context = mockk<Context>(relaxed = true)
     private val intent = mockk<Intent>()
+    private val sonarIdProvider = mockk<SonarIdProvider>()
     private val applicationComponent = mockk<ApplicationComponent>(relaxed = true)
     private val sut = BootCompletedReceiver()
 
@@ -26,14 +32,49 @@ class BootCompletedReceiverTest {
     fun setUp() {
         every { context.appComponent } returns applicationComponent
         every { intent.action } returns Intent.ACTION_BOOT_COMPLETED
-        sut.sonarIdProvider = mockk(relaxed = true)
+        mockkObject(BluetoothService)
+        every { BluetoothService.start(context) } returns Unit
+
+        sut.sonarIdProvider = sonarIdProvider
         sut.reminderManager = reminderManager
     }
 
     @Test
-    fun onReceive() {
+    fun onReceiveOtherIntentActionFinishes() {
+        every { intent.action } returns "SOME_OTHER_ACTION"
+
+        verifyAll {
+            sonarIdProvider wasNot Called
+            reminderManager wasNot Called
+        }
+    }
+
+    @Test
+    fun onReceiveNotifiesReminderManager() {
+        every { sonarIdProvider.hasProperSonarId() } returns false
+
         sut.onReceive(context, intent)
 
         verify(exactly = 1) { reminderManager.handleBootComplete() }
+    }
+
+    @Test
+    fun onReceiveIfSonarIdIsPresentStartsBluetoothService() {
+        every { sonarIdProvider.hasProperSonarId() } returns true
+
+        sut.onReceive(context, intent)
+
+        verify(exactly = 1) { BluetoothService.start(context) }
+    }
+
+    @Test
+    fun onReceiveIfSonarIdIsAbsentDoesNotStartBluetoothService() {
+        every { sonarIdProvider.hasProperSonarId() } returns false
+
+        sut.onReceive(context, intent)
+
+        verify {
+            BluetoothService wasNot Called
+        }
     }
 }
