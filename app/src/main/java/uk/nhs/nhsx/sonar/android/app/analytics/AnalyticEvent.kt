b36add961def95c0291c510179cee3dc3dc98973
Tom Brand <tbrand@pivotal.io>
2020-05-01 16:16:56 +0100

    Document analitic events
    Signed-off-by: Anon 24 <anon24@example.com>

---
+++ app/src/main/java/uk/nhs/nhsx/sonar/android/app/analytics/AnalyticEvent.kt
@@ -0,0 +1,60 @@
+/*
+ * Copyright © 2020 NHSX. All rights reserved.
+ */
+
+package uk.nhs.nhsx.sonar.android.app.analytics
+
+fun registrationSucceeded(): AnalyticEvent = AnalyticEvent(
+    "Registration succeeded"
+)
+
+fun registrationSendTokenCallFailed(statusCode: Int?): AnalyticEvent = AnalyticEvent(
+    "Registration failed",
+    mapOf(
+        "Reason" to "Registration call failed",
+        "Status code" to statusCode.toString()
+    )
+)
+
+fun registrationFailedWaitingForFCMToken(): AnalyticEvent = AnalyticEvent(
+    "Registration failed",
+    mapOf(
+        "Reason" to "No FCM token"
+    )
+)
+
+fun registrationFailedWaitingForActivationNotification(): AnalyticEvent = AnalyticEvent(
+    "Registration failed",
+    mapOf(
+        "Reason" to "Activation notification not received"
+    )
+)
+
+fun registrationFailedActivationCallFailed(statusCode: Int?): AnalyticEvent = AnalyticEvent(
+    "Registration failed",
+    mapOf(
+        "Reason" to "Activation call failed",
+        "Status code" to statusCode.toString()
+    )
+)
+
+fun partialPostcodeProvided(): AnalyticEvent = AnalyticEvent(
+    "Partial postcode provided"
+)
+
+fun onboardingCompleted(): AnalyticEvent = AnalyticEvent(
+    "Onboarding completed"
+)
+
+fun collectedContactEvents(yesterday: Int, all: Int): AnalyticEvent = AnalyticEvent(
+    "Collected contact events",
+    mapOf(
+        "Yesterday" to yesterday.toString(),
+        "All" to all.toString()
+    )
+)
+
+data class AnalyticEvent(
+    val name: String,
+    val properties: Map<String, String>? = null
+)
