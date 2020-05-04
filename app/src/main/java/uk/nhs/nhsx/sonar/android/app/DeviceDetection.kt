6ce4bf45f4cab715560150dbc1241965dbd795a1
Anon 19 <anon19@example.com>
2020-05-04 15:12:55 +0100

    Add comment on unsupported device detection

--- app/src/main/java/uk/nhs/nhsx/sonar/android/app/DeviceDetection.kt
+++ app/src/main/java/uk/nhs/nhsx/sonar/android/app/DeviceDetection.kt
@@ -13,8 +13,14 @@ class DeviceDetection(
     // without supporting MultiAdvertisement feature (isMultipleAdvertisementSupported == false)
     // This allows for them to run without crashing but probably not advertise as expected.
     // See: https://stackoverflow.com/a/32096285/952041
-    // Here is the previous logic:
+    // Here is part of the previous device detection logic that blocked Huawei P Smart phone.
+    // After commenting it out we are not sure if the phone's advertising works
+    // with our Bluetooth scanning process:
     //  || (bluetoothAdapter.isEnabled && !bluetoothAdapter.isMultipleAdvertisementSupported)
+    // TODO: We need a real device to test Bluetooth scanning if isMultipleAdvertisementSupported == false
+    // TODO: We need analytics to identify number of devices that fall into this bucket
+    // and if they scan correctly
+
     fun isUnsupported(): Boolean =
         bluetoothAdapter == null || !packageManager.hasSystemFeature(FEATURE_BLUETOOTH_LE) ||
             (bluetoothAdapter.isEnabled && bluetoothAdapter.bluetoothLeAdvertiser == null)
