620d324ab332e0844cc9470a5c4ed8a1a44a7069
Anon 3 <anon3@example.com>
2020-05-13 16:25:07 +1000

    Add publishing of pacts to pipeline

--- .github/actions/build-action/entrypoint.sh
+++ .github/actions/build-action/entrypoint.sh
@@ -12,12 +12,14 @@ validate "$INPUT_GOOGLE_SERVICES" "googleServices"
 validate "$INPUT_SONAR_BASE_URL" "sonarBaseUrl"
 validate "$INPUT_SONAR_HEADER_VALUE" "sonarHeaderValue"
 validate "$INPUT_SONAR_ANALYTICS_KEY" "sonarAnalyticsKey"
+validate "$INPUT_PACTBROKER_URL" "pactbrokerUrl"
 
 GOOGLE_SERVICES_FILE=app/google-services.json
 mkdir build
 echo "$INPUT_GOOGLE_SERVICES" > $GOOGLE_SERVICES_FILE
 
-./gradlew build packageDebugAndroidTest \
+./gradlew build packageDebugAndroidTest pactPublish \
     -Psonar.baseUrl="$INPUT_SONAR_BASE_URL" \
     -Psonar.headerValue="$INPUT_SONAR_HEADER_VALUE" \
-    -Psonar.analyticsKey="$INPUT_SONAR_ANALYTICS_KEY"
+    -Psonar.analyticsKey="$INPUT_SONAR_ANALYTICS_KEY" \
+    -Ppactbroker.url="$INPUT_PACTBROKER_URL"
