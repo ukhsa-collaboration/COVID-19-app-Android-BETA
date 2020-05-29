799b4f6575dc513ee2e296c6038f55c2f0d6598b
Anon 15 <anon15@example.com>
2020-05-29 06:51:58 -0500

    [#173039425] Onboarding screen no longer starts a new task
    Signed-off-by: Anon 21 <anon21@example.com>

--- app/src/main/java/uk/nhs/nhsx/sonar/android/app/onboarding/MainOnboardingActivity.kt
+++ app/src/main/java/uk/nhs/nhsx/sonar/android/app/onboarding/MainOnboardingActivity.kt
@@ -37,8 +37,5 @@ class MainOnboardingActivity : ColorInversionAwareActivity() {
 
         fun getIntent(context: Context) =
             Intent(context, MainOnboardingActivity::class.java)
-                .apply {
-                    flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-                }
     }
 }
