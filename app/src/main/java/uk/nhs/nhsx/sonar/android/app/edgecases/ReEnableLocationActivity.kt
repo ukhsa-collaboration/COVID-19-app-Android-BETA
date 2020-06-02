8a16126866e19c70734201ef9d82ca0306cccdc9
Anon 23 <anon23@example.com>
2020-06-02 17:03:07 +0100

    [#173103715] Break down text paragraphs for ReEnableLocationActivity

--- app/src/main/java/uk/nhs/nhsx/sonar/android/app/edgecases/ReEnableLocationActivity.kt
+++ app/src/main/java/uk/nhs/nhsx/sonar/android/app/edgecases/ReEnableLocationActivity.kt
@@ -9,9 +9,9 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.core.view.isVisible
 import kotlinx.android.synthetic.main.activity_edge_case.banner
-import kotlinx.android.synthetic.main.activity_edge_case.edgeCaseText
 import kotlinx.android.synthetic.main.activity_edge_case.edgeCaseTitle
 import kotlinx.android.synthetic.main.activity_edge_case.nhsPanel
+import kotlinx.android.synthetic.main.activity_edge_case.paragraphContainer
 import uk.nhs.nhsx.sonar.android.app.R
 import uk.nhs.nhsx.sonar.android.app.onboarding.EnableLocationActivity
 
@@ -23,9 +23,11 @@ class ReEnableLocationActivity : EnableLocationActivity() {
         nhsPanel.isVisible = false
 
         edgeCaseTitle.setText(R.string.re_enable_location_title)
-        val appName = getString(R.string.app_name)
-        val rationale = getString(R.string.re_enable_location_rationale, appName)
-        edgeCaseText.text = rationale
+        paragraphContainer.addAllParagraphs(
+            getString(R.string.re_enable_location_rationale_p1),
+            getString(R.string.re_enable_location_rationale_p2),
+            getString(R.string.re_enable_location_rationale_p3, getString(R.string.app_name))
+        )
     }
 
     override fun onBackPressed() {
