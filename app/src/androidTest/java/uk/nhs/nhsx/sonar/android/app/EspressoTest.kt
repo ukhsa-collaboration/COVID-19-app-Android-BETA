f692c736ebc7ec7947ebf9329212a52fe7e96046
Anon 23 <anon23@example.com>
2020-06-10 11:48:36 +0100

    Move grant rule to Espresso base class

--- app/src/androidTest/java/uk/nhs/nhsx/sonar/android/app/EspressoTest.kt
+++ app/src/androidTest/java/uk/nhs/nhsx/sonar/android/app/EspressoTest.kt
@@ -1,10 +1,17 @@
 package uk.nhs.nhsx.sonar.android.app
 
+import androidx.test.rule.GrantPermissionRule
 import org.junit.After
 import org.junit.Before
+import org.junit.Rule
 import uk.nhs.nhsx.sonar.android.app.testhelpers.TestApplicationContext
+import uk.nhs.nhsx.sonar.android.app.util.AndroidLocationHelper
 
 abstract class EspressoTest {
+
+    @get:Rule
+    val permissionRule: GrantPermissionRule =
+        GrantPermissionRule.grant(*AndroidLocationHelper.requiredLocationPermissions)
 
     protected lateinit var testAppContext: TestApplicationContext
 
